892b9491899144da1dea19081224e3d36e859577
FacundoVaca <facundo_vacabarboza@hotmail.com>
2023-06-01 17:24:42 -0300

    modificado

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtenemos referencias a los elementos del DOM
+  let btnResumen = document.querySelector("#btnResumen");
+  let btnBorrar = document.querySelector("#btnBorrar");
+  let outputTicket = document.querySelector("#outputTicket");
+
+  // Agregamos los event listeners a los botones
+  btnResumen.addEventListener("click", calcularTotal);
+  btnBorrar.addEventListener("click", borrarOutput);
+
+  // Resto del código...
+
+  // Función para calcular el total a pagar
+  function calcularTotal() {
+    // Resto del código...
+  }
+
+  // Función para borrar el output
+  function borrarOutput() {
+    // Resto del código...
+  }
+});
